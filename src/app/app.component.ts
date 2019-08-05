b6a596b63b8d44dcbc39d322af82cfcb68885eaa
Partical777 <z25993021@gmail.com>
2019-08-05 20:47:16 +0800

    HTTP-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HeroService } from './hero.service';
 
+import LocalJSON from "../assets/data/dashboard-link.json";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +14,7 @@ import { HeroService } from './hero.service';
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes: Hero[];
+  localJson = LocalJSON;
 
   constructor(public messageService: MessageService, private heroService: HeroService) {}
 
